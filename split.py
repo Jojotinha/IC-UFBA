16a69a9f994303ca59f2a8c2b88e5ba27575180a
Jojotinha <95496135+Jojotinha@users.noreply.github.com>
2022-11-13 12:49:02 -0300

    Delete split.py

--- split.py
+++
@@ -1,8 +0,0 @@
-num = input('Digite seus valores\n>>>')
-list1 = num.split()
-list2 = []
-for i in list1:
-    n = float(i)
-    list2.append(n)
-mean = str(sum(list2)/len(list2)).replace('.',',')
-print('A média é igual: {}'.format(mean))
